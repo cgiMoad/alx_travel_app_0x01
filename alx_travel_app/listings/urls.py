b7d72f56ed474d1d7debae727cdc3d972f8a728a
cgiMoad <151995207+cgiMoad@users.noreply.github.com>
2025-08-02 14:01:13 +0100

    Update urls.py

--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,17 +1,11 @@
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
 
-schema_view = get_schema_view(
-   openapi.Info(
-      title="Travel API",
-      default_version='v1',
-      description="API for managing listings and bookings",
-   ),
-   public=True,
-   permission_classes=[permissions.AllowAny],
-)
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
 
-urlpatterns += [
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+urlpatterns = [
+    path('', include(router.urls)),
 ]
